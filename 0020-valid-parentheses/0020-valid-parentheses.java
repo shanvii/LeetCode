3c885dd7e70d12aefb0323f0ba572aa1c6c9b9fd
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-07-30 23:53:07 +0530

    Time: 2 ms (52.97%), Space: 41.5 MB (43.68%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() <= 1)
-            return false;
-
+        
         Stack<Character> stack = new Stack<>();
 
         for(int i = 0 ; i < s.length(); i++) {
