c38f5c19507b06bc315c99f0a8a689507356d760
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-07-30 23:50:43 +0530

    Time: 2 ms (52.97%), Space: 41.4 MB (60.79%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() <= 1)
+            return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0 ; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[')
+                stack.push(ch);
+            else {
+                if(stack.isEmpty())
+                    return false;
+                    
+                if((stack.peek() == '(' && ch == ')') || (stack.peek() == '{' && ch == '}') || (stack.peek() == '[' && ch == ']'))
+                    stack.pop();
+                else
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
