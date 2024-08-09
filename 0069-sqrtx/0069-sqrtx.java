184be380d9449978f65dd6906006ac053b0a375c
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-08-10 01:16:15 +0530

    Time: 1 ms (86.01%), Space: 41 MB (25.06%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,11 +1,24 @@
 class Solution {
     public int mySqrt(int x) {
         if(x <= 1)  return x;
-        long i = 1, sq = i*i;
-        while(sq <= x) {
-            i++;
-            sq = i*i;
+
+        // long i = 1, sq = i*i;
+        // while(sq <= x) {
+        //     i++;
+        //     sq = i*i;
+        // }
+        // return (int)i-1;
+ 
+        long l = 1;
+        long r = x;
+        while(l <= r) {
+            long mid = l + (r - l)/2;
+            long sq = mid * mid;
+
+            if(sq == x) return (int)mid;
+            else if(sq < x) l = mid + 1;
+            else    r = mid - 1;
         }
-        return (int)i-1;
+        return (int)r;
     }
 }
