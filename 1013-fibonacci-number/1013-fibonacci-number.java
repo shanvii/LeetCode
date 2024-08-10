3d9b8fe1be4a1a0baa704fe439dfc8167ad8ac56
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-08-11 00:46:29 +0530

    Time: 9 ms (24.93%), Space: 40.7 MB (6.05%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,16 +1,20 @@
+// class Solution {
+//     public int fib(int n) {
+//         if(n == 0) return 0;
+//         if(n == 1) return 1;
+//         int a = 0, b = 1;
+//         int sum = 0;
+//         for(int i = 2; i <= n; i++) {
+//         sum = a + b;
+//         a = b;
+//         b = sum;
+//         }
+//         return sum;
+//     }
+// }
+
 class Solution {
     public int fib(int n) {
-        // if(n == 0) return 0;
-        // if(n == 1) return 1;
-        // int a = 0, b = 1;
-        // int sum = 0;
-        // for(int i = 2; i <= n; i++) {
-        // sum = a + b;
-        // a = b;
-        // b = sum;
-        // }
-        // return sum;
-
         int sum = 0;
         if (n == 0)
             return 0;
