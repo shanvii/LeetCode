919fb26164afc9a3a59fc3784fe6b67deb7b3182
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-08-13 20:02:23 +0530

    Time: 0 ms (100%), Space: 41.2 MB (87.45%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,15 +1,28 @@
 class Solution {
     public String toLowerCase(String s) {
-        String res = "";
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')
-                res += (char) (s.charAt(i) + 32);
-            else
-                res += s.charAt(i);
+        StringBuilder res = new StringBuilder();
+        for(char c: s.toCharArray()) {
+            if(c >= 'A' && c <= 'Z') {
+                c = (char) (c + 32);
+            }
+            res.append(c);
         }
-        return res;
+        return res.toString();
     }
 }
+
+// class Solution {
+//     public String toLowerCase(String s) {
+//         String res = "";
+//         for(int i = 0; i < s.length(); i++) {
+//             if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')
+//                 res += (char) (s.charAt(i) + 32);
+//             else
+//                 res += s.charAt(i);
+//         }
+//         return res;
+//     }
+// }
 
 // class Solution {
 //     public String toLowerCase(String s) {
