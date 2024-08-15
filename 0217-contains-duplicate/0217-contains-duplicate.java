be1ab5e6323b3891513315208f03b7c77c5ed430
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-08-15 16:03:32 +0530

    Time: 10 ms (89.28%), Space: 58.2 MB (44.75%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,9 +1,21 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length-1; i++) {
-            if(nums[i] == nums[i+1])    return true;
+        HashSet<Integer> num = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(num.contains(nums[i]))
+                return true;
+            num.add(nums[i]);
         }
         return false;
     }
 }
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         for(int i = 0; i < nums.length-1; i++) {
+//             if(nums[i] == nums[i+1])    return true;
+//         }
+//         return false;
+//     }
+// }
