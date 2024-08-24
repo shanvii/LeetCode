10059dc92d4cf8e90fa5dca0ddc2c510978570d0
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-08-24 17:15:16 +0530

    Time: 3 ms (99.69%), Space: 68.2 MB (47.55%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,22 +8,56 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         if (head == null || head.next == null)  return true;
+        
+//         ArrayList<Integer> arr = new ArrayList<Integer>();
+//         while(head != null) {
+//             arr.add(head.val);
+//             head = head.next;
+//         }
+//         int low = 0;
+//         int high = arr.size() - 1;
+//         while(low < high) {
+//             if(arr.get(low) != arr.get(high))   return false;
+//             low ++;
+//             high --;
+//         }
+//         return true;
+//     }
+// }
+
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null)  return true;
         
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        while(head != null) {
-            arr.add(head.val);
-            head = head.next;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        int low = 0;
-        int high = arr.size() - 1;
-        while(low < high) {
-            if(arr.get(low) != arr.get(high))   return false;
-            low ++;
-            high --;
+
+        fast = head;
+
+        ListNode prev = null;
+        while(slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
         }
+
+        slow = prev;
+
+        while(slow != null) {
+            if(fast.val != slow.val)    return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
+
         return true;
     }
 }
