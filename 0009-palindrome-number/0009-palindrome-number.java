a2a448f428cebe7158b1b054b0f41e4734b052f7
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-08-24 20:20:35 +0530

    Time: 4 ms (100%), Space: 44.3 MB (12.23%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,30 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0)
-            return false;
-        String num = Integer.toString(x);
-        int i = 0;
-        int j = num.length() - 1;
-
-        while(i < j) {
-            if(num.charAt(i) != num.charAt(j))
-                return false;
-            i++; j--;
+        int rev = 0;
+        int copy = x;
+        while(copy > 0) {
+            int digit = copy % 10;
+            rev = rev * 10 + digit;
+            copy /= 10;
         }
-        return true;
+        return rev == x;
     }
 }
+
+
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         if(x < 0)
+//             return false;
+//         String num = Integer.toString(x);
+//         int i = 0;
+//         int j = num.length() - 1;
+
+//         while(i < j) {
+//             if(num.charAt(i) != num.charAt(j))
+//                 return false;
+//             i++; j--;
+//         }
+//         return true;
+//     }
+// }
