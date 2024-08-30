6254d8d360107d0eec651ff5f81a40cb3ad6f304
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-08-31 02:52:28 +0530

    Time: 5 ms (64.04%), Space: 44.7 MB (12.85%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -2,12 +2,34 @@ class Solution {
     public boolean judgeCircle(String moves) {
         int i = 0, j = 0;
         for(int k = 0; k < moves.length(); k++) {
-            char ch = moves.charAt(k);
-            if(ch == 'R')   j++;
-            else if(ch == 'L')  j--;
-            else if(ch == 'U')  i--;
-            else i++;
+            switch(moves.charAt(k)) {
+                case 'U':
+                    i--;
+                    break;
+                case 'D':
+                    i++;
+                    break;
+                case 'L':
+                    j++;
+                    break;
+                default:
+                    j--;
+            }
         }
         return (i == 0 && j == 0);
     }
 }
+
+// class Solution {
+//     public boolean judgeCircle(String moves) {
+//         int i = 0, j = 0;
+//         for(int k = 0; k < moves.length(); k++) {
+//             char ch = moves.charAt(k);
+//             if(ch == 'R')   j++;
+//             else if(ch == 'L')  j--;
+//             else if(ch == 'U')  i--;
+//             else i++;
+//         }
+//         return (i == 0 && j == 0);
+//     }
+// }
