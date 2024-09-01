45b0cd0a4ff6c5f49e8bf3e6ce0c520c838027b1
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-09-02 00:18:36 +0530

    Time: 0 ms (100%), Space: 42.1 MB (72.9%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -5,14 +5,9 @@ class Solution {
         
         while (left < right) {
             int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > nums[mid + 1]) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+            if (nums[mid] > nums[mid + 1])  right = mid;
+            else  left = mid + 1;
         }
-        
         return left;
     }
 }
