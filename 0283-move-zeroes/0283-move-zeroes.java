8ced6e6ddc40b93b1f07d3a8ab023d7d1e3ea951
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-09-02 00:30:49 +0530

    Time: 2 ms (82.65%), Space: 46.1 MB (33.22%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -10,6 +10,6 @@ class Solution {
         int k = 0;
         for(int i = 0; i < nums.length; i++)
             if(nums[i] != 0)    nums[k++] = nums[i];
-        while(k < nums.length && k >= 0)    nums[k++] = 0;
+        while(k < nums.length)    nums[k++] = 0;
     }
 }
