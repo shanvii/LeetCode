5aa62985bc49987ba0296bada714e78ac5c618b4
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-09-03 01:38:26 +0530

    Time: 2 ms (72.4%), Space: 56.6 MB (99.37%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -1,12 +1,31 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        long sum = 0;
-        for(int c: chalk)   sum += c;
-        k %= sum;
-        for(int i = 0; i < chalk.length; i++) {
-            if(chalk[i] > k)    return i;
-            k -= chalk[i];
+        long sum[] = new long[chalk.length];
+        sum[0] = chalk[0];
+        for(int i = 1; i < chalk.length; i++) {
+            sum[i] = sum[i-1] + chalk[i];
         }
-        return -1;
+        k %= sum[chalk.length - 1];
+        int low = 0, high = chalk.length - 1;
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            if(sum[mid] == k)   return mid+1;
+            if(sum[mid] > k)  high = mid;
+            else low = mid + 1;
+        }
+        return low;
     }
 }
+
+// class Solution {
+//     public int chalkReplacer(int[] chalk, int k) {
+//         long sum = 0;
+//         for(int c: chalk)   sum += c;
+//         k %= sum;
+//         for(int i = 0; i < chalk.length; i++) {
+//             if(chalk[i] > k)    return i;
+//             k -= chalk[i];
+//         }
+//         return -1;
+//     }
+// }
