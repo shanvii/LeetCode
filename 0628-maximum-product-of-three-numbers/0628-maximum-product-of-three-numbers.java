da2e56e0e94d4e83d253a458911b8afaf91f0693
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2024-09-17 12:53:08 +0530

    Time: 4 ms (86.52%), Space: 45.9 MB (7.38%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,9 +1,41 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length - 1;
-        int prd1 = nums[n] * nums[n-1] * nums[n-2];
-        int prd2 = nums[0] * nums[1] * nums[n];
-        return Math.max(prd1, prd2);
+        int min1, min2, max1, max2, max3;
+        min1 = min2 = Integer.MAX_VALUE;
+        max1 = max2 = max3 = Integer.MIN_VALUE;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(max1 < nums[i]) {
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if(max2 < nums[i]) {
+                max3 = max2;
+                max2 = nums[i];
+            }
+            else if(max3 < nums[i]) {
+                max3 = nums[i];
+            }
+
+            if(min1 > nums[i]) {
+                min2 = min1;
+                min1 = nums[i];
+            }
+            else if(min2 > nums[i]) {
+                min2 = nums[i];
+            }
+        }
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
     }
 }
+
+// class Solution {
+//     public int maximumProduct(int[] nums) {
+//         Arrays.sort(nums);
+//         int n = nums.length - 1;
+//         int prd1 = nums[n] * nums[n-1] * nums[n-2];
+//         int prd2 = nums[0] * nums[1] * nums[n];
+//         return Math.max(prd1, prd2);
+//     }
+// }
