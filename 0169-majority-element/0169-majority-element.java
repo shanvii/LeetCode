a93015311b4fe139b87996e0c50af9343667af5e
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2025-01-15 12:55:53 +0530

    Time: 1 ms (99.8%), Space: 53.3 MB (35.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,10 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
+        int count = 0, candidate = 0;
+        for(int num: nums) {
+            if(count == 0)  candidate = num;
+            if(num == candidate)    count++;
+            else    count--;
+        }
+        return candidate;
     }
 }
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Arrays.sort(nums);
+//         int n = nums.length;
+//         return nums[n/2];
+//     }
+// }
 
 // class Solution {
 //     public int majorityElement(int[] nums) {
