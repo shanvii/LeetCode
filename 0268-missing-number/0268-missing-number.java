2dcbd4e35f0a9ad643dcd2302c5c2cb9c3a3ab27
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2025-01-20 11:53:52 +0530

    Time: 0 ms (100%), Space: 45.3 MB (53.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,8 +1,19 @@
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int sum = nums.length;
+//         for(int i = 0; i < nums.length; i++)   {
+//             sum += i - nums[i];
+//         }
+//         return sum;
+//     }
+// }
+
 class Solution {
     public int missingNumber(int[] nums) {
-        int sum = nums.length;
+        int n = nums.length;
+        int sum = (n * (n + 1)) / 2;
         for(int i = 0; i < nums.length; i++)   {
-            sum += i - nums[i];
+            sum -= nums[i];
         }
         return sum;
     }
