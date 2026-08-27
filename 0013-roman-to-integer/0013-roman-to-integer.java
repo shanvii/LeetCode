44d0430f1add8be32c173e51f517fbd02986f016
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2026-08-28 03:55:46 +0530

    Time: 5 ms (32.25%), Space: 47 MB (9.42%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,40 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
+        int res = 0;
 
-        int result = 0;
+        Map<Character, Integer> map = new HashMap<>();
 
-        Map<Character, Integer> hash = new HashMap<>();
+        map.put('M', 1000);
+        map.put('D', 500);
+        map.put('C', 100);
+        map.put('L', 50);
+        map.put('X', 10);
+        map.put('V', 5);
+        map.put('I', 1);
 
-        hash.put('M', 1000);
-        hash.put('D', 500);
-        hash.put('C', 100);
-        hash.put('L', 50);
-        hash.put('X', 10);
-        hash.put('V', 5);
-        hash.put('I', 1);
-
-        // way-1
-        // for (int i = 0; i < s.length() - 1; i++) {
-        //     if (hash.get(s.charAt(i)) < hash.get(s.charAt(i + 1)))
-        //         result -= hash.get(s.charAt(i));
-        //     else
-        //         result += hash.get(s.charAt(i));
-        // }
-        // return result + hash.get(s.charAt(s.length() - 1));
-
-        // way-2
-        s = s.replace("IV", "IIII");
-        s = s.replace("IX", "VIIII");
-        s = s.replace("XL", "XXXX");
-        s = s.replace("XC", "LXXXX");
-        s = s.replace("CD", "CCCC");
-        s = s.replace("CM", "DCCCC");
-
-        for(int i = 0; i < s.length(); i++) {
-            result += hash.get(s.charAt(i));
+        for(int i = 0; i < s.length() - 1; i++) {
+            if(map.get(s.charAt(i)) < map.get(s.charAt(i+1)))
+                res -= map.get(s.charAt(i));
+            else
+                res += map.get(s.charAt(i));
         }
 
-        return result;
-        
+        res += map.get(s.charAt(s.length() - 1));
+        return res;
     }
 }
