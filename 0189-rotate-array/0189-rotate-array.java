c83ed74f8866cd7916900883ee70175d09adca88
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2026-08-31 02:04:01 +0530

    Time: 4 ms (25.79%), Space: 268.3 MB (19.9%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -5,6 +5,9 @@ class Solution {
 
         k %= nums.length;
 
+        if(k == 0)
+            return;
+
         reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k - 1);
         reverse(nums, k, nums.length - 1);
